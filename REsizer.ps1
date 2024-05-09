93bf4a817a3580dd50a0747c5d838e8193065395
Tanner Harkin <91706744+tannerharkin@users.noreply.github.com>
2024-05-09 12:26:30 -0700

    Added logo to script

--- REsizer.ps1
+++ REsizer.ps1
@@ -18,6 +18,27 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.T NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #>
 
+# Print logo
+Write-Output ""
+Write-Output "     ###################     ######################             ######                                                  "
+Write-Output "     ##                #### ###                  ##            ##    ##                                                 "
+Write-Output "    ###                  ## ##                  ###            ##    ##                                                 "
+Write-Output "    ##                    ####                  ############    #######################     #########       ##### ##### "
+Write-Output "    ##      ########      ###      ################        ### ##    ###              ## ####       ####  ###   ###   ##"
+Write-Output "   ##      ###    ##      ###      ##############             ###    ##               ####             ## ##          ##"
+Write-Output "   ##      ########       ##                   #    #######  ####    ###########     ###     #######    ####       #### "
+Write-Output "   ##                    ###                  ##    ##### #### ##    ##      ##     ###     #########    ###     ##     "
+Write-Output "  ##                   #####                  ##        #####  ##    ##    ###     ####    ###########   ###    ##      "
+Write-Output "  ##                #### ##      #################           ####    ##   ##     ### ##                   ##    ##      "
+Write-Output " ###     #####      ##   ##      ##              ######       ###    ##  ##     ##   ##    #################    ##      "
+Write-Output " ##      ## ##       ## ##       ###################   ###     ##    ####     ###    ##     ##      ##### ##    ##      "
+Write-Output "###      ##  ##      #####                  ####   #######     ##    ###     ###########     ########   ####    ##      "
+Write-Output "##      ##   ###      ###                   ####              ###    ##               ####              ####    ##      "
+Write-Output "##     ###    ##      ####                 ### ###         ### ##    ###              ## ###          ### ###   ##      "
+Write-Output " #######        #######  ###################      #########      ##### ################     ##########      #####   v1.0"
+Write-Output ""
+Write-Output ""
+
 # Print the disclaimer
 Write-Output "DISCLAIMER: This script is provided 'as is' with no guarantees or warranties, either expressed or implied. Use at your own risk."
 Write-Output ""
